507335ecb9521acdb9f4d58b8e5337fa691786f5
unknown <ghaliarose89@gmail.com>
2021-09-17 09:32:38 -0500

    showquestion function

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -8,19 +8,43 @@ var answer = document.querySelector('#answercontainer');
 var results = document.querySelector('#result');
 var body=document.body;
 var score = 0;
+var questionNumber = 0;
+var intervalSeconds = 90;
+var intervalHold = 0;
+var intervalPenalty = 10;
+var answer1btn=document.getElementById('answer1');
+var answer2btn=document.getElementById('answer2');
+var answer3btn=document.getElementById('answer3');
+var answer4btn=document.getElementById('answer4');
+var initalInput= document.getElementById('inital');
+var intialbtn=document.getElementById('initalbtn');
+var highscorebtn=document.getElementById('highscroebtn');
+var highscorearr= [];
 var questions =
 [
-    {   q:"How do you call a function named myFunction?" },
-    {   q:"How to write an IF statement for executing some code if i is NOT equal to 5?"},   
-    {   q:"How does a WHILE loop start?"},
-    {   q: "How can you add a comment in a JavaScript?" },
-    {   q: "What is the correct way to write a JavaScript array?"},
+    {   q:"How do you call a function named myFunction?",
+        correct: "myFunction()",
+    },
+    {   q:"How to write an IF statement for executing some code if i is NOT equal to 5?",
+        correct:  "if (i!=5)",
+    },   
+    {   q:"How does a WHILE loop start?",
+        correct:  "while (i<=10)",
+    },
+
+    {   q: "How can you add a comment in a JavaScript?",
+        correct: "// This is a comment",
+    },
+    {   q: "What is the correct way to write a JavaScript array?",
+        correct:"var colors = ['red','green', 'blue']",
+    },
 ];
 var answers = [
     {
         "A": "call function myFunction()",
         "B":  "call myFunction()",
         "C": "myFunction()",
+        "D": "MYFUNCTION()",
     },
 
     {   "A": "if i =!5 then",
@@ -33,194 +57,140 @@ var answers = [
     {   "A": "while i = 1",
         "B": "while i = 1 to 10 ",
         "C": "while (i<=10)" ,
+        "D": "While.Starter (i=1)",
 
-    },
+
+    },  
 
     {
-        "A": "while (i <= 10; i++)",
+        "A": "comment: this is a comment",
         "B": "// This is a comment",
         "C": "<!-- This is a comment -->",
         "D": "This is a comment  ",
     },
 
     {
-        "A": "var colos = 'red','green', 'blue' ",
+        "A": "var colors = 'red','green', 'blue' ",
         "B": "var colors = ['red','green', 'blue']",
         "C": "var colors = 1 = ('red'), 2= ('green'), 3 = ('blue')",
         "D": "var colors = (1:'red', 2:'green' 3: 'blur')",
     },
     ]
     
+    answer1btn.style.display='none';
+    answer2btn.style.display='none';
+    answer3btn.style.display='none';
+    answer4btn.style.display='none';
+    initalInput.style.display='none';
+    intialbtn.style.display='none';
+    highscorebtn.style.display='none';
 
- function showquestions1(event){
-        event.preventDefault();
+
+
+ function showquestion(){
+        
         quizinfo.textContent="";
         startbtnEl.remove();
-         questionQuiz.textContent= questions[0].q; 
-         var optionA = document.createElement("button");
-         optionA.textContent= answers[0].A;
-         var optionB = document.createElement("button");
-         optionB.textContent= answers[0].B;
-         var optionC = document.createElement("button");
-         optionC.textContent= answers[0].C;
-         answer.appendChild(optionA);
-         answer.appendChild(optionB);
-         answer.appendChild(optionC);
-         optionA.className="answer-btn";
-         optionB.className="answer-btn";
-         optionC.className="answer-btn";
-         var result = document.createElement("h3");
-         result.className= "result";
-        result.textContent="Result";
-        results.appendChild(result);
-        var next=document.querySelector(".answer-btn");
-        next.addEventListener("click", function(){
-            result.textContent="Wrong";
-             score++;
-            console.log(score);
-            answer.innerHTML = "";
-          });
-          next.addEventListener("click",  showquestions2);
-    };
+        answer1btn.style.display='';
+        answer2btn.style.display='';
+        answer3btn.style.display='';
+        answer4btn.style.display='';
+
+        console.log("questionNumber= "+ questionNumber);
+        questionQuiz.textContent= questions[questionNumber].q; 
+        answer1btn.textContent= answers[questionNumber].A;
+        answer2btn.textContent= answers[questionNumber].B;
+        answer3btn.textContent= answers[questionNumber].C;
+        answer4btn.textContent= answers[questionNumber].D;
+        answer1btn.addEventListener("click",function(event){
+            event.preventDefault();
+            var x = event.target.innerText;
+            checkAnswer(x);
+        });
+        answer2btn.addEventListener("click",function(event){
+            event.preventDefault();
+            var x = event.target.innerText;
+            checkAnswer(x);
+        });
+        answer3btn.addEventListener("click",function(event){
+            event.preventDefault();
+            var x = event.target.innerText;
+            checkAnswer(x);
+        });
+        answer4btn.addEventListener("click",function(event){
+            event.preventDefault();
+            var x = event.target.innerText;  
+            checkAnswer(x);
+        });
+    
         
+    };
+
+function checkAnswer(y){
+        console.log(y);
+           //using if statement to check the correct answer
+         if (y === questions[questionNumber].correct){
+            results.textContent="Correct!";
+            score++;
+            questionNumber++;
+         }
+
+        else{
         
+            questionNumber++;
+            intervalSeconds = intervalSeconds - 5;
+            results.textContent="Wrong!";
+         }
 
-    function showquestions2(event){
-        event.preventDefault();
-        
-         questionQuiz.textContent= questions[1].q; 
-         var optionA = document.createElement("button");
-         optionA.textContent= answers[1].A;
-         var optionB = document.createElement("button");
-         optionB.textContent= answers[1].B;
-         var optionC = document.createElement("button");
-         optionC.textContent= answers[1].C;
-         answer.appendChild(optionA);
-         answer.appendChild(optionB);
-         answer.appendChild(optionC);
-         optionA.className="answer-btn";
-         optionB.className="answer-btn";
-         optionC.className="answer-btn";
-         var result = document.createElement("h3");
-         body.appendChild(result);
-         result.className= "result";
-        result.textContent="Result";
-        var next=document.querySelector(".answer-btn");
-         next.addEventListener("click", function(){
-            showquestions3;
-            result.textContent="Wrong";
-             score++;
-            console.log(score);});
-    };
+        if (questionNumber >= questions.length){
+            quizOver();
+        }
+        else {
+            
+            showquestion();
+        }
 
-    function showquestions3(event){
-        event.preventDefault();
-        answerscontainer.innerHTML = "";
-        
-         questionQuiz.textContent= questions[2].q; 
-         var answerscontainer = document.createElement("div");
-         body.appendChild(answerscontainer);
-         var optionA = document.createElement("button");
-         optionA.textContent= answers[2].A;
-         var optionB = document.createElement("button");
-         optionB.textContent= answers[2].B;
-         var optionC = document.createElement("button");
-         optionC.textContent= answers[2].C;
-         answerscontainer.appendChild(optionA);
-         answerscontainer.appendChild(optionB);
-         answerscontainer.appendChild(optionC);
-         optionA, optionB, optionC.className="answer-btn";
-         var result = document.createElement("h3");
-         body.appendChild(result);
-         result.className= "result";
-         result.textContent="Result";
-        var next=document.querySelector(".answer-btn");
-         next.addEventListener("click", function(){
-            showquestions4;
-            answer.remove();
-            result.textContent="Wrong";
-             score++;
-            console.log(score);});
-    };
+       
+};
 
-    function showquestions4(event){
-        event.preventDefault();
-        quizinfo.textContent="";
-        startbtnEl.remove();
-         questionQuiz.textContent= questions[3].q; 
-         var answerscontainer = document.createElement("div");
-         body.appendChild(answerscontainer);
-         var optionA = document.createElement("button");
-         optionA.textContent= answers[3].A;
-         var optionB = document.createElement("button");
-         optionB.textContent= answers[3].B;
-         var optionC = document.createElement("button");
-         optionC.textContent= answers[3].C;
-         answerscontainer.appendChild(optionA);
-         answerscontainer.appendChild(optionB);
-         answerscontainer.appendChild(optionC);
-         optionA.className="answer-btn";
-         optionB.className="answer-btn";
-         optionC.className="answer-btn";
-         var result = document.createElement("h3");
-         body.appendChild(result);
-         result.className= "result";
-        result.textContent="Result";
-        var next=document.querySelector(".answer-btn");
-         next.addEventListener("click", function(){
-            showquestions5;
-            answerscontainer.innerHTML = "";
-            result.textContent="Wrong";
-             score++;
-            console.log(score);});
-    };
+ function quizOver(){
+    answer1btn.style.display='none';
+    answer2btn.style.display='none';
+    answer3btn.style.display='none';
+    answer4btn.style.display='none';
+    initalInput.style.display='';
+    intialbtn.style.display='';
+    highscorebtn.style.display='';
+    intervalSeconds = 0;
+    results.textContent="";
+    questionQuiz.textContent= "Quiz is Over!  your score is " + score + "out of "+ questions.length;
+    intialbtn.textContent = 'Enter Inital';
+    intialbtn.addEventListener('click',highScore());
+    highscorebtn.textContent = 'High Score';
+    
+};
 
-    function showquestions5(event){
-        event.preventDefault();
-        quizinfo.textContent="";
-        startbtnEl.remove();
-         questionQuiz.textContent= questions[4].q; 
-         var answerscontainer = document.createElement("div");
-         body.appendChild(answerscontainer);
-         var optionA = document.createElement("button");
-         optionA.textContent= answers[4].A;
-         var optionB = document.createElement("button");
-         optionB.textContent= answers[4].B;
-         var optionC = document.createElement("button");
-         optionC.textContent= answers[4].C;
-         answerscontainer.appendChild(optionA);
-         answerscontainer.appendChild(optionB);
-         answerscontainer.appendChild(optionC);
-         optionA.className="answer-btn";
-         optionB.className="answer-btn";
-         optionC.className="answer-btn";
-         var result = document.createElement("h3");
-         body.appendChild(result);
-         result.className= "result";
-        result.textContent="Result";
-        var next=document.querySelector(".answer-btn");
-         next.addEventListener("click", function(){
-            showquestions2;
-            answerscontainer.remove();
-            result.textContent="Wrong";
-             score++;
-            console.log(score);});
-    };
+function highScore(){
 
+    highscorearr.push(initalInput.textContent);
+    localStorage.setItem ('highscorearr',JSON.stringify(highscorearr));
 
-// function countdown() {
-//     var timeLeft = 90;
-  
-//     var timeInterval = setInterval(function() {
-      
-//       timerEl.textContent = timeLeft + ' seconds left';
-//       timeLeft--;
-//       if(timeLeft === 0) {
-//         clearInterval(timeInterval);
-//       }}
-//       //
-//     },1000)
-
-
-
-startbtnEl.addEventListener('click', showquestions1);
+}
+    
+startbtnEl.addEventListener('click', function(event){
+    event.preventDefault();
+    showquestion();
+    if (intervalHold === 0) {
+        intervalHold = setInterval(function () {
+            intervalSeconds--;
+            timerEl.textContent = "Time: " + intervalSeconds;
+             if (intervalSeconds <= 0) {
+                clearInterval(intervalHold);
+                timerEl.textContent = "Time's up!";
+                quizOver();
+            }
+        }, 1000);
+    }
+   
+}
+);
